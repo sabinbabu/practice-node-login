8b86d6636d1770c30e03a46d62e1bc1e3afd1a3d
sabinbabu <sabinacharya444@gmail.com>
2025-02-10 13:26:17 +1100

    GET Route for signup added

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
+const _dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.send(`<a href='/login'>Login</a><a href='/signup'>Sign Up</a>`);
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(`${_dirname}/signup.html`);
 });
 
 app.listen(PORT, (error) => {
