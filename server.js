34c95a8b496e9b5c59a7e746ac5ac8e6f979d0d7
sabinbabu <sabinacharya444@gmail.com>
2025-02-10 13:22:50 +1100

    GET route for home path added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send(`<a href='/login'>Login</a><a href='/signup'>Sign Up</a>`);
+});
+
 app.listen(PORT, (error) => {
   error
     ? console.log(error)
